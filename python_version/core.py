0b55d83139b9cc11c8ffd208311bc5d7785425f2
Markopolo141 <markopolo141@github.com>
2016-12-01 12:14:14 -0800

    smaller code

--- python_version/core.py
+++ python_version/core.py
@@ -1,9 +1,6 @@
-from sympy import symbols
-from sympy.utilities.lambdify import lambdify
 from copy import deepcopy as copy
 import numpy
-import sympy
-from utils import *
+import math
 
 '''
 given a switch-matrix, generate the vectors of the pure (or extreme) strategies.
@@ -28,3 +25,54 @@ def generate_switch_extrema(switch):
         else:
             ex = [e+[None] for e in ex]
     return ex
+
+
+
+'''
+given a dictionary defining ranges of iterables in form 
+{"count1":{"min":0,"max":5,"step":1},...} return a list of dicts of all combinations of thoes values
+note: "step" is optional on each
+'''
+def multi_iterate(d):
+    keys = d.keys()
+    amalgum_list = []
+    def sub_method(vals,i):
+        if i<len(keys):
+            count = d[keys[i]]["min"]
+            while count <= d[keys[i]]["max"]:
+                sub_method(vals + [count],i+1)
+                count += d[keys[i]].get("step",1)
+        else:
+            amalgum_list.append(dict(zip(keys,vals)))
+    sub_method([],0)
+    return amalgum_list
+
+'''
+given square pymatrix M, use power-iterations to find largest positive eigenvalue and its corresponding eigenvector.
+i and ip control the number of iterations taken place: the number of iterations is given by: i*2^ip
+'''
+def eigen(M, ip, i):
+    size = M.shape[0]
+    v = numpy.matrix([[1.0/size] for s in range(size)])
+    for count in range(ip):
+        M = M*M
+    for count in range(i):
+        v = M*v
+        s = v.sum()
+        if s == 0:
+            return 0,numpy.matrix([[1.0/size] for s in range(size)])
+        if math.isnan(s):
+            raise Exception("overflow: power_iteration power factor is too big")
+        z = 1.0/s
+        v = v * z
+    return math.pow(s, 1.0/(2**(ip))),v
+
+'''
+
+'''
+def matrix_map(M,f):
+    MM = [list(a) for a in M]
+    for i in range(len(MM)):
+        for j in range(len(MM[0])):
+            MM[i][j] = f(M[i][j],i,j)
+    return MM
--- python_version/sim.py
+++ python_version/sim.py
@@ -1,9 +1,10 @@
+from core import *
+from tqdm import tqdm
 import json
 import click
 import operator
-from utils import *
-from core import *
-from tqdm import tqdm
+import numpy
+import sympy
 from sympy import symbols
 from sympy.parsing.sympy_parser import parse_expr
 from sympy.utilities.lambdify import lambdify
@@ -35,26 +36,21 @@ def simulate(config):
         subs = {"Z":{"min":0,"max":1,"step":1}}
     subs = {symbols(a):b for a,b in subs.iteritems()}
     def switch_weighter(vector):
-        return lambda x,i,j: vector[j].pop() if x and vector[j] is not None else 0
+        return lambda x,i,j: x if vector[j] is None else vector[j].pop() if x else 0
     extrema = [numpy.matrix(matrix_map(switch,switch_weighter(e))) for e in generate_switch_extrema(switch)]
     pop_symbols = [sympy.symbols("s{}".format(i)) for i in range(len(choice))]
     
     output_json = []
     for sub_values in tqdm(multi_iterate(subs)):
-        
         pops = [a.copy() for a in origional_pops]
         subs_choice = matrix_map(choice,lambda x,i,j:lambdify(pop_symbols,x.subs(sub_values)))
-        import pdb
-        pdb.set_trace()
         for i in range(iterations):
             for p in range(len(pops)):
                 flattened_pop = [a[0,0] for a in pops[p]]
                 weighted_choice = numpy.matrix(matrix_map(subs_choice,lambda x,i,j:x(*flattened_pop)))
                 extrema_eigen_pairs = [eigen(weighted_choice*e,i_ip,i_i) for e in extrema]
-                extrema_growths = [e[0] for e in extrema_eigen_pairs]
-                max_extrema_index, growth = max(enumerate(extrema_growths), key=operator.itemgetter(1))
                 z = sequence(i)
-                pops[p] = pops[p]*(1-z) + extrema_eigen_pairs[max_extrema_index][1]*z
+                pops[p] = pops[p]*(1-z) + max(extrema_eigen_pairs, key=operator.itemgetter(0))[1]*z
         output_json.append({"parameters":dict([("{}".format(str(iii[0])),"{0:.3f}".format(iii[1])) for iii in sub_values.iteritems()]),"pops":[[[1 if pp.sum()>0.05 else 0 for pp in p],[ppp.sum() for ppp in p]] for p in pops]})
         out_file = file(output_formatter, "w")
         out_file.write(json.dumps({"output":output_json},sort_keys=True).replace('{"parameters":','\n{"parameters":'))
